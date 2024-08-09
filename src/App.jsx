f4b1fad8bb465c7e1fb82d8c70d873721f6f69f9
aijazbinqasim <aijazbinqasim@hotmail.com>
2024-08-09 20:41:07 +0500

    pass func to chid component as prop.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-import TodoList from './TodoList';
+import TodoList from "./TodoList";
 
 const App = () => {
+  const handleClickTodoItemInfo = (id, title, completed) => {
+    alert(
+      `Todo List Details: \n\n ID - ${id}\n Title - ${title}\n Is completed - ${
+        completed ? "Yes" : "No"
+      }`
+    );
+  };
+
   const todos = [
-    { id: 1, title: 'Learn React', completed: true },
-    { id: 2, title: 'Build an app', completed: false },
-    { id: 3, title: 'Deploy the app', completed: false },
+    { id: 1, title: "Learn React", completed: false },
+    { id: 2, title: "Build an app", completed: false },
+    { id: 3, title: "Deploy the app", completed: false },
+    { id: 4, title: "Learn C#", completed: false },
+    { id: 5, title: "Build C# app", completed: false },
+    { id: 6, title: "Deploy C# app", completed: false },
+    { id: 7, title: "Learn Dart", completed: false },
+    { id: 8, title: "Build an Dart app", completed: false },
+    { id: 9, title: "Deploy Dart app", completed: false },
+    { id: 10, title: "Learn more", completed: false },
   ];
 
   return (
     <main>
       <h1>Todo List</h1>
-      <TodoList todos={todos} />
+      <hr />
+      <ol>
+        <TodoList todos={todos} onClickTodoItemInfo={handleClickTodoItemInfo} />
+      </ol>
     </main>
   );
 };
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,17 +1,20 @@
-const TodoList = ({ todos }) => {
-    const renderedTodos = todos.map(({id,title,completed}) => {
+const TodoList = (props) => {
+  const renderedTodos = props.todos.map((item) => {
+    return (
+      <li
+        style={{ padding: 10, fontWeight: "bold", cursor: "pointer" }}
+        onClick={() =>
+          props.onClickTodoItemInfo(item.id, item.title, item.completed)
+        }
+        key={item.id}
+      >
+        {item.completed ? " ✔ " : ""}
+        {item.title}
+      </li>
+    );
+  });
 
-    //  if(completed){
-    //     return <div key={id}>✔ {title}</div>
-    //  }
+  return <>{renderedTodos}</>;
+};
 
-      return (<div key={id}>
-         {completed ? '✔' : ''}
-        {title}</div>
-      );
-    });
-  
-    return <section>{renderedTodos}</section>;
-  };
-  
-  export default TodoList;
+export default TodoList;
